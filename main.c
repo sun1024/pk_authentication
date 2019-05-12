bbba4edbfff2013b290a5fb42c153871dd0cbabc
sun1024 <sunnanbin@gmail.com>
2019-05-12 19:18:24 +0800

    sk encrypt pk decrypt

--- main.c
+++ main.c
@@ -40,6 +40,8 @@ static void print_bytes(uint8_t *data, size_t size);
 static void write_bytes(const char *path, uint8_t *data, size_t size);
 static char *my_encrypt(char *str,char *path_key);
 static char *my_decrypt(char *str,char *path_key);
+static char *my_sk_encrypt(char *str, char *path_key);
+static char *my_pk_decrypt(char *str, char *path_key);
 
 int main(int argc, char **argv)
 {
@@ -397,6 +399,96 @@ void write_bytes(const char *path, uint8_t *data, size_t size)
     if(RSA_private_decrypt(256, (unsigned char*)str, (unsigned char*)p_de, p_rsa, RSA_PKCS1_PADDING) < 0)
     {
         perror("RSA_private_decrypt() error ");
+        goto End;
+    }
+
+    End:
+    //6.释放秘钥空间， 关闭文件
+     if(p_rsa)    RSA_free(p_rsa);
+    if(file)     fclose(file);
+
+    return p_de;
+}
+//私钥加密
+ char *my_sk_encrypt(char *str, char *path_key)
+ {
+     char *p_en = NULL;
+     RSA  *p_rsa = NULL;
+     FILE *file = NULL;
+
+     int  lenth = 0;    //flen为源文件长度， rsa_len为秘钥长度
+
+     //1.打开秘钥文件
+     if((file = fopen(path_key, "rb")) == NULL)
+     {
+        perror("fopen() error 111111111 ");
+         goto End;
+     }        
+
+     //2.从私钥中获取 加密的秘钥
+     if((p_rsa = PEM_read_RSAPrivateKey(file, NULL,NULL,NULL )) == NULL)
+     {
+         ERR_print_errors_fp(stdout);
+         goto End;
+     }
+     lenth = strlen(str);
+
+     p_en = (char *)malloc(256);
+     if(!p_en)
+     {
+         perror("malloc() error 2222222222");
+         goto End;
+     }    
+     memset(p_en, 0, 256);
+
+     //5.对内容进行加密
+     if(RSA_private_encrypt(lenth, (unsigned char*)str, (unsigned char*)p_en, p_rsa, RSA_PKCS1_PADDING) < 0)
+     {
+     perror("RSA_private_encrypt() error 2222222222");
+     goto End;
+     }
+ End:
+
+     //6.释放秘钥空间， 关闭文件
+     if(p_rsa)    RSA_free(p_rsa);
+     if(file)     fclose(file);
+
+     return p_en;
+ } 
+//公钥解密
+ char *my_pk_decrypt(char *str, char *path_key)
+ {
+     char *p_de = NULL;
+     RSA  *p_rsa = NULL;
+     FILE *file = NULL;
+
+    //1.打开秘钥文件
+    file = fopen(path_key, "rb");
+    if(!file)
+    {
+        perror("fopen() error 22222222222");
+        goto End;
+    }        
+
+    //2.从公钥中获取 解密的秘钥
+    if((p_rsa = PEM_read_RSA_PUBKEY(file, NULL,NULL,NULL )) == NULL)
+    {
+        ERR_print_errors_fp(stdout);
+        goto End;
+    }
+
+    p_de = (char *)malloc(245);
+    if(!p_de)
+    {
+        perror("malloc() error ");
+        goto End;
+    }    
+    memset(p_de, 0, 245);
+
+    //5.对内容进行加密
+    if(RSA_public_decrypt(256, (unsigned char*)str, (unsigned char*)p_de, p_rsa, RSA_PKCS1_PADDING) < 0)
+    {
+        perror("RSA_public_encrypt() error ");
         goto End;
     }
 
